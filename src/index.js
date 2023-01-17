d9171483a34c4c201af71020b63e067c0767a2dc
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2023-01-17 23:59:56 +0300

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -38,19 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-  const dashesAndDotArr = expr
-    .match(/.{1,10}/g)
-    .map((i) => i.match(/.{1,2}/g))
-    .map((it) => {
-      const arr = [];
-      it.forEach((item) => {
-        if (item === "10") arr.push(".");
-        if (item === "11") arr.push("-");
-      });
-      return arr.join("");
-    });
-  const res = dashesAndDotArr.map((i) => (i === "" ? " " : MORSE_TABLE[i])).join("");
-  return res;
+  let regexp1 = /\S{10}/gi;
+  let input = expr.match(regexp1);
+  let array = [],
+    array2 = [],
+    array3 = [];
+  let result = "";
+  input.forEach((item) => {
+    let index = item.split("").findIndex((element) => element === "1");
+    console.log(item);
+    if (index > 0) array.push(item.slice(index));
+    else array.push(item);
+  });
+  let regexp2 = /\S{2}/gi;
+  array.forEach((item) => {
+    array2.push(item.match(regexp2));
+  });
+  array2.forEach((item) => {
+    array3.push(item.join("").replaceAll("10", ".").replaceAll("11", "-"));
+  });
+  let keys = Object.keys(MORSE_TABLE);
+  let values = Object.values(MORSE_TABLE);
+  array3.forEach((item) => {
+    for (let j = 0; j < keys.length; j++) {
+      if (item === keys[j]) {
+        result += values[j];
+      }
+    }
+  });
+  return result;
 }
 
 module.exports = {
